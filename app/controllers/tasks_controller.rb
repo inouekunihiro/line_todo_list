5bfebb9e5f33b78511e1115fb5e7ef9e1b69e74e
kuni <kunihiro1010@gmail.com>
2020-12-13 23:13:50 +0900

    model と controller を追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,73 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    # @task = Task.new(task_params)
+
+    # インスタンスを model から作成
+
+    @task = Task.new(task_params)
+
+    # インスタンスを DB に保存する
+    @task.save!
+
+    # json として値を返す
+    render :show
+
+    # if @task.save
+    #   render :show, status: :created, location: @task
+    # else
+    #   render json: @task.errors, status: :unprocessable_entity
+    # end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    # 対象のレコードを探す
+    # @task = Task.find(params[:id])
+    # それをupdate
+    @task.update!(task_params)
+    # json として値を返す
+    render :show
+
+    # if @task.update(task_params)
+    #   render :show, status: :ok, location: @task
+    # else
+    #   render json: @task.errors, status: :unprocessable_entity
+    # end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy!
+
+    render :show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:task)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: "tasks#index"
+  # root "tasks#index" でもOK
+  post "callback", to: "line_bot#callback"
+  resources :tasks
+  # resources :line_bot
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
